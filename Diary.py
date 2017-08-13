a77ed547befaa339411f60741a23886c0d0e6f11
Karan Joshi <karanjoshi31@live.com>
2017-08-13 12:28:44 -0500

    finished adding and searching logic

--- Diary.py
+++ Diary.py
@@ -1,6 +1,7 @@
 import datetime
-
+import sys
 from collections import OrderedDict
+
 from peewee import *
 
 db = MySQLDatabase('dev',user="root",password='kjoshi31')
@@ -18,13 +19,13 @@ class Entry(Model):
 
 
 def initialize():
-    """ create db and table if they dont exist"""
+    """Create db and table if they dont exist"""
     db.connect()
     db.create_tables([Entry],safe = True)
 
 
 def menu_loop():
-    """ show the menu """
+    """Show the menu """
     choice = None
 
     while choice != 'q':
@@ -37,18 +38,46 @@ def menu_loop():
             menu[choice]()
 
 def add_entry():
-    """ Add an entry """
+    """Add an entry"""
+    print("Enter your entry. press ctrl+d when finished")
+    data = sys.stdin.read().strip()
 
-def view_entries():
-    """ View previoes entries"""
+    if data:
+        if input('Save Entry? [Y/n] ').lower() != 'n':
+            Entry.create(content = data)
+            print("Saved successfully!")
+
+def view_entries(search_query=None):
+    """View previoes entries"""
+    entries = Entry.select().order_by(Entry.timestamp.desc())
+
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
+    for entry in entries:
+        timestamp = entry.timestamp.strftime('%A %B %d %Y %I:%M%p')
+        print(timestamp)
+        print('='*len(timestamp))
+        print(entry.content)
+        print('n) next entry')
+        print('q) return to main menu' )
+
+        next_action = input('Action: [N/q]').lower().strip()
+        if next_action == 'q':
+            break
+
+def search_entries():
+    """Search entries for a string"""
+    view_entries(input("Search query: "))
 
 def delete_entry(entry):
-    """ Delete an entry """
+    """Delete an entry """
 
 
 menu = OrderedDict([
     ('a',add_entry),
     ('v',view_entries),
+    ('s', search_entries)
 ])
 
 
